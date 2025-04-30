860019204bcb4f50f3cc0e8ce3572b9d5aabd3da
emirberbic <emir.berbic9@gmail.com>
2025-04-30 14:33:22 +0200

    Update index.ts

--- supabase/functions/stripe-checkout/index.ts
+++ supabase/functions/stripe-checkout/index.ts
@@ -1,6 +1,6 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
 import Stripe from 'stripe';
-import { createClient } from 'npm:@supabase/supabase-js@2.49.1';
+import { createClient } from '@supabase/supabase';
 
 const supabase = createClient(Deno.env.get('SUPABASE_URL') ?? '', Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '');
 const stripeSecret = Deno.env.get('STRIPE_SECRET_KEY')!;
